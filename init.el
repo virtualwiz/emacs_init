dabc794296d153f148b9f731c13666ae78b1f48a
Matthew S Du <matthewdu@protonmail.com>
2019-08-08 18:25:17 +0100

    add octave support add font settings

--- init.el
+++ init.el
@@ -2,28 +2,41 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (w3m monokai-theme which-key iedit auto-complete restart-emacs magit counsel ivy-hydra ivy try)))
+    (reverse-theme pdf-tools w3m which-key iedit auto-complete restart-emacs magit counsel ivy-hydra ivy try)))
+ '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 143 :width normal)))))
 
 ;; configure auto-complete
 (ac-config-default)
 
+;; configure ispell
+(setq ispell-dictionary "british")
+
 ;; configure which-key
 (which-key-mode)
+
+;; configure linum
+(global-linum-mode)
+(add-hook 'pdf-view-mode-hook (lambda() (linum-mode -1)))
+
+;; configure PDF tools
+(pdf-tools-install)
+
+;; configure Octave mode
+(setq auto-mode-alist
+      (cons '("\\.m$" . octave-mode) auto-mode-alist))
+(add-hook 'octave-mode-hook
+          (lambda ()
+	    (auto-complete-mode 1)
+	    ))
 
 ;; configure ivy
 (ivy-mode 1)
@@ -47,5 +60,13 @@
 ;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
 ;; (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
-;; load theme
-(load-theme 'monokai t)
+;; configure UI
+(load-theme 'reverse t)
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
+(set-frame-font "Terminus (TTF)-13.5" nil t)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Terminus (TTF)" :foundry "unknown" :slant normal :weight normal :height 136 :width normal)))))
